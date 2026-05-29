41991cd36fbd26d18c7401eeec53cd38af1d43fa
MaxPce <max.ponce@tecsup.edu.pe>
2026-05-29 14:47:05 -0500

    29-5

--- src/competitions/dto/generate-tennis-phases.dto.ts
+++ src/competitions/dto/generate-tennis-phases.dto.ts
@@ -1,22 +1,29 @@
-import {
-  IsArray,
-  IsEnum,
-  IsNumber,
-} from 'class-validator';
+// generate-tennis-phases.dto.ts
+import { IsArray, IsEnum, IsString, IsInt, ArrayNotEmpty, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export enum TennisGenerationMode {
-  WITH_MATCHES = 'with_matches',
-  PHASES_ONLY  = 'phases_only',
+export enum TennisPhaseFormat {
+  SINGLE_ELIMINATION = 'single_elimination',
+  ROUND_ROBIN = 'round_robin',
+  BEST_OF_3 = 'best_of_3',
+}
+
+class TennisPhaseGroupDto {
+  @IsString()
+  name: string;
+
+  @IsEnum(TennisPhaseFormat)
+  format: TennisPhaseFormat;
+
+  @IsArray()
+  @IsInt({ each: true })
+  registrationIds: number[];
 }
 
 export class GenerateTennisPhasesDto {
-  @IsNumber()
-  eventCategoryId: number;
-
-  @IsEnum(TennisGenerationMode)
-  mode: TennisGenerationMode;
-
   @IsArray()
-  @IsNumber({}, { each: true })
-  registrationIds: number[];
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => TennisPhaseGroupDto)
+  groups: TennisPhaseGroupDto[];
 }
